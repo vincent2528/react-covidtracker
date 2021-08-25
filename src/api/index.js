28cbf04c00c05f5760eafb8f4ed6c524f3ee015b
vincent2528 <krutikgambhir@gmail.com>
2021-08-25 22:22:43 +0530

    second

--- src/api/index.js
+++ src/api/index.js
@@ -34,24 +34,6 @@ export const fetchDailyData = async () => {
   }
 };
 
-// Instead of Global, it fetches the daily data for the US
-// export const fetchDailyData = async () => {
-//   try {
-//     const { data } = await axios.get(
-//       "https://api.covidtracking.com/v1/us/daily.json"
-//     );
-
-//     return data.map(({ positive, recovered, death, dateChecked: date }) => ({
-//       confirmed: positive,
-//       recovered,
-//       deaths: death,
-//       date,
-//     }));
-//   } catch (error) {
-//     return error;
-//   }
-// };
-
 export const fetchCountries = async () => {
   try {
     const {
